db2453edc7edfbab0c50a58f551a8aa89d0b9e69
honjow <honjow311@gmail.com>
2022-09-18 16:25:58 +0800

    add FlutterAndroidVolumeKeydownFragmentActivity

---
+++ android/src/main/java/dev/darttools/flutter_android_volume_keydown/FlutterAndroidVolumeKeydownFragmentActivity.java
@@ -0,0 +1,24 @@
+package dev.darttools.flutter_android_volume_keydown;
+
+import static android.view.KeyEvent.KEYCODE_VOLUME_DOWN;
+import static android.view.KeyEvent.KEYCODE_VOLUME_UP;
+import static dev.darttools.flutter_android_volume_keydown.FlutterAndroidVolumeKeydownPlugin.eventSink;
+
+import android.view.KeyEvent;
+
+import io.flutter.embedding.android.FlutterFragmentActivity;
+
+public class FlutterAndroidVolumeKeydownFragmentActivity extends FlutterFragmentActivity {
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KEYCODE_VOLUME_DOWN && eventSink != null) {
+            eventSink.success(true);
+            return true;
+        }
+        if(keyCode == KEYCODE_VOLUME_UP && eventSink != null) {
+            eventSink.success(false);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
